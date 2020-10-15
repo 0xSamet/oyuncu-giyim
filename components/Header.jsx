7838f48ef0aa0c141a6f372492cca8f82ed7095a
samet <osmansamet58@gmail.com>
2020-10-15 23:07:40 +0300

    progress

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 import CartIcon from "../public/icons/cart.svg";
 import ProfileIcon from "../public/icons/profile.svg";
+import NotificationIcon from "../public/icons/notification.svg";
 
 export default function Header() {
   const [searchWord, setSearchWord] = useState("");
@@ -12,7 +13,7 @@ export default function Header() {
         <img className="logo" src="/oyuncu-giyim-logo.png" />
       </div>
       <div className="header-right row">
-        <div className="header-right-left col-md-6">
+        <div className="header-right-left col-md-10">
           <Input
             placeholder="Ürün Ara"
             loading={searchWord !== ""}
@@ -21,8 +22,9 @@ export default function Header() {
             }}
           />
         </div>
-        <div className="header-right-right col-md-6">
+        <div className="header-right-right col-md-2">
           <div className="header-right-icon-wrapper">
+            <NotificationIcon className="notification-icon" />
             <ProfileIcon className="profile-icon" />
             <CartIcon />
           </div>
