2e456960ccce40cc6dffc37a53ae6270d2a1a972
samet <osmansamet58@gmail.com>
2020-10-16 19:51:03 +0300

    progress

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,13 +4,25 @@ import { useState } from "react";
 import CartIcon from "../public/icons/cart.svg";
 import ProfileIcon from "../public/icons/profile.svg";
 import NotificationIcon from "../public/icons/notification.svg";
+import MasterCardIcon from "../public/icons/mastercard.svg";
+import VisaCardIcon from "../public/icons/visacard.svg";
 
 export default function Header() {
   const [searchWord, setSearchWord] = useState("");
   return (
     <footer>
-      <div>
-        <h2>Footer</h2>
+      <div className="footer-left">
+        <p>Copyright © 2019, Oyuncu Giyim, Tüm Hakları Saklıdır.</p>
+      </div>
+      <div className="footer-right">
+        <div className="informations-wrapper">
+          <span>Gizlilik Sözleşmesi</span>
+          <span>Çerezler</span>
+        </div>
+        <div className="payments-wrapper">
+          <MasterCardIcon className="mastercard-icon" />
+          <VisaCardIcon />
+        </div>
       </div>
     </footer>
   );
