0e3ddbabc8298f4991db5ab424926fa9c243c732
samet <osmansamet58@gmail.com>
2020-10-29 16:59:23 +0300

    progress

--- components/pages/Layout.jsx
+++ components/pages/Layout.jsx
@@ -10,6 +10,13 @@ const Layout = ({ children, title = "Oyuncu Giyim" }) => {
           name="viewport"
           content="initial-scale=1.0, width=device-width, minimal-ui"
         />
+        <meta name="apple-mobile-web-app-capable" content="yes"></meta>
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="#ffffff"
+        ></meta>
+        <meta name="apple-mobile-web-app-title" content="Oyuncu Giyim"></meta>
+        <meta name="application-name" content="Oyuncu Giyim"></meta>
       </Head>
       {children}
     </>
