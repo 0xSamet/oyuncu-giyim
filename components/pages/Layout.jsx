4173492dc226b709893a25f3dca829c4a6077f51
samet <osmansamet58@gmail.com>
2020-10-29 16:52:12 +0300

    progress

--- components/pages/Layout.jsx
+++ components/pages/Layout.jsx
@@ -6,7 +6,10 @@ const Layout = ({ children, title = "Oyuncu Giyim" }) => {
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="viewport"
+          content="initial-scale=1.0, width=device-width, minimal-ui"
+        />
       </Head>
       {children}
     </>
